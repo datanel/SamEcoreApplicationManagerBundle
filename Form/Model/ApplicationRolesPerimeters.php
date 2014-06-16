7ad184e40edde4f56935b747127c024f2e816715
kzi <kevin.ziemianski@canaltp.fr>
2014-06-16 17:45:27 +0200

    Clean code

--- Form/Model/ApplicationRolesPerimeters.php
+++ Form/Model/ApplicationRolesPerimeters.php
@@ -9,21 +9,18 @@ class ApplicationRolesPerimeters
     
     public function __get($name)
     {
-//        debug($name . ' get');
         $getMethod = 'get' . ucfirst($name);
         return $this->application->$getMethod();
     }
     
     public function __set($name, $arguments)
     {
-//        debug($name . ' set');
         $setMethod = 'set' . ucfirst($name);
         return call_user_func(array($this->application, $setMethod), $arguments);
     }
     
     public function __call($name, $arguments) {
         if (preg_match('/^(set|get|add)/', $name)) {
-//            debug($name . ' __cal');
             return call_user_func(array($this->application, $name), $arguments);
         }
     }
